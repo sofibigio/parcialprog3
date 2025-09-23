dcdf9a40fbfc7d0f4f1d4aad4bd591918049449d
ambaralianak <aambar@udesa.edu.ar>
2025-09-22 22:52:26 -0300

    terminado favoritos

--- src/Screens/Favoritos/Favoritos.js
+++ src/Screens/Favoritos/Favoritos.js
@@ -8,32 +8,156 @@ import Favorito from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header"
 
 let apikey = 'd39a94778431335cc790439556f16732'
+
+
 class Favoritos extends Component {
-   constructor(props){
+  constructor(props){
     super(props);
     this.state = {
       peliculas: [],
       series: []
-      }}
+    };
+  }
+
+  leerLista(clave){
+    const texto = localStorage.getItem(clave);
+    if (!texto) {
+      return [];
+    }
+    try {
+      const lista = JSON.parse(texto);
+      if (lista) {
+        return lista;
+      } else {
+        return [];
+      }
+    } catch (e) {
+      return [];
+    }
+  }
+
+  componentDidMount(){
+    this.cargarFavoritos();
+  }
+
+  cargarFavoritos(){
+    const idsPeliculas = this.leerLista("pelisFav");
+    const idsSeries    = this.leerLista("seriesFav");
+
+    this.setState({ peliculas: [], series: [] });
+
+  
+    idsPeliculas.map((idPeli) => {
+      const url = "https://api.themoviedb.org/3/movie/" + idPeli + "?api_key=" + apikey;
+      fetch(url)
+        .then(function(respuesta){ return respuesta.json(); })
+        .then((data) => {
+          if (data) {
+            if (data.id) {
+              const copia = this.state.peliculas.map(function(item){ return item; });
+              copia.push(data);
+              this.setState({ peliculas: copia });
+            }
+          }
+        })
+        .catch(function(){});
+      return null;
+    });
 
 
+    idsSeries.map((idSerie) => {
+      const url = "https://api.themoviedb.org/3/tv/" + idSerie + "?api_key=" + apikey;
+      fetch(url)
+        .then(function(respuesta){ return respuesta.json(); })
+        .then((data) => {
+          if (data) {
+            if (data.id) {
+              const copia = this.state.series.map(function(item){ return item; });
+              copia.push(data);
+              this.setState({ series: copia });
+            }
+          }
+        })
+        .catch(function(){});
+      return null;
+    });
+  }
+
+  borrarDeFavoritos(idContenido, seccion){
+    const esPeli = (seccion === "peliculas") ? true : false;
+    const clave  = esPeli ? "pelisFav" : "seriesFav";
 
 
-  render(){
-     return (
+    const idsActualizados = this.leerLista(clave).filter(function(guardado){
+      return guardado !== idContenido;
+    });
+    localStorage.setItem(clave, JSON.stringify(idsActualizados));
+
+
+    const listaActual = esPeli ? this.state.peliculas : this.state.series;
+    const listaNueva  = listaActual.filter(function(elemento){
+      return elemento.id !== idContenido;
+    });
+
+    if (esPeli) {
+      this.setState({ peliculas: listaNueva });
+    } else {
+      this.setState({ series: listaNueva });
+    }
+}
+render(){
+    return (
       <div className="container">
-        <Header/>
+        <Header />
         <h1>Favoritos</h1>
 
-       
-        <Footer/>
+        <div>
+          <h2>Películas favoritas</h2>
+          <div>
+            {this.state.peliculas.map((item) => {
+              const nombre = item.title;
+              const poster = item.poster_path ? ("https://image.tmdb.org/t/p/w500" + item.poster_path) : "";
+              return (
+                <div key={"peliculas-" + item.id}>
+                  <Link to={"/detalle/movie/" + item.id}>
+                    {poster ? <img src={poster} alt={nombre} /> : ""}
+                  </Link>
+                  <p>{nombre}</p>
+                  <button onClick={() => this.borrarDeFavoritos(item.id, "peliculas")}>
+                    Eliminar
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div>
+          <h2>Series favoritas</h2>
+          <div>
+            {this.state.series.map((item) => {
+              const nombre = item.name;
+              const poster = item.poster_path ? ("https://image.tmdb.org/t/p/w500" + item.poster_path) : "";
+              return (
+                <div key={"series-" + item.id}>
+                  <Link to={"/detalle/tv/" + item.id}>
+                    {poster ? <img src={poster} alt={nombre} /> : ""}
+                  </Link>
+                  <p>{nombre}</p>
+                  <button onClick={() => this.borrarDeFavoritos(item.id, "series")}>
+                    Eliminar
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <Footer />
       </div>
-    )
+    );
   }
-   
 }
-  
-  
 
-
+   
 export default withRouter(Favoritos)
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,8 +3,8 @@ import './Card.css'
 import { Link } from 'react-router-dom'
 import { jsx } from "react/jsx-runtime";
 
-class Card extends Component{
-    constructor(props){
+class Card extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             mostrarDescripcion: false,
@@ -14,104 +14,123 @@ class Card extends Component{
             favs: "Sacar de favoritos"
         }
     }
+componentDidMount(){
+  const id = this.props.data.id;
+  const esPeli = (this.props.tipo == "movie");
+  const clave  = esPeli ? "pelisFav" : "seriesFav";
 
-    manejarDescripcion(){
-        if(this.state.mostrarDescripcion == false){
-            this.setState({mostrarDescripcion: true, textoBoton: 'Ocultar descripcion'})
+  const texto = localStorage.getItem(clave);
+  if (!texto) { return; }
+
+  try {
+    const lista = JSON.parse(texto);
+    const yaEsta = lista.filter(function(item){ return item == id; }).length > 0;
+    if (yaEsta) {
+      this.setState({
+        esFavorito: true,
+        textoFavorito: "Sacar de favoritos"
+      });
+    }
+  } catch(e){}
+}
+
+
+    manejarDescripcion() {
+        if (this.state.mostrarDescripcion == false) {
+            this.setState({ mostrarDescripcion: true, textoBoton: 'Ocultar descripcion' })
         } else {
-            this.setState({mostrarDescripcion: false, textoBoton: 'Mostrar descripcion'})
+            this.setState({ mostrarDescripcion: false, textoBoton: 'Mostrar descripcion' })
         }
     }
 
 
     botonFavorito() {
+        const id = this.props.data.id;
+        const esPeli = (this.props.tipo == "movie");
+
         let favSeries = localStorage.getItem("seriesFav")
         let favPelis = localStorage.getItem("pelisFav")
+
         if (this.state.esFavorito === false) {
-            
-        
-        if (favSeries === null) {
-            let series = []
-            series.push("id de la serie")
-            let favString = JSON.stringify(series)
-            localStorage.setItem("seriesFav", favString)
-        }else{
-            let parseado = JSON.parse(favSeries)
-            parseado.push("el id de la series")
-            let favString = JSON.stringify(parseado)
-            localStorage.setItem("seriesFav", favString)
-        }
-        this.setState({
-            esFavorito: true,
-            textoFavorito: "Sacar de favoritos"
-        })
-       
-        if(this.state.esFavorito === false){
-             let parseado = JSON.parse(favSeries)
-             let nuevoFiltro = parseado.filter(ids => ids != "al ide de la serie")
-            let favString = JSON.stringify(nuevoFiltro)
-            localStorage.setItem("seriesFav", favString)
-
-            this.setState({
-            esFavorito: false,
-            textoFavorito: "🩶"
-        })            
-        }
-        
-
-        //hacer lo mismo con pelis
-          if (favPelis === null) {
-            let pelis = []
-            series.push("id de las peliculas")
-            let favString = JSON.stringify(pelis)
-            localStorage.setItem("pelisFav", favString)
-        }else{
-            let parseado = JSON.parse(favPelis)
-            parseado.push("el id de las peliculas")
-            let favString = JSON.stringify(parseado)
-            localStorage.setItem("pelisFav", favString)
-        }
-        this.setState({
-            esFavorito: true,
-            textoFavorito: "Sacar de favoritos"
-        })
-       
-        if(this.state.esFavorito === false){
-             let parseado = JSON.parse(favPelis)
-             let nuevoFiltro = parseado.filter(ids => ids != "al ide de la pelicula")
-            let favString = JSON.stringify(nuevoFiltro)
-            localStorage.setItem("pelisFav", favString)
-
-            this.setState({
-            esFavorito: false,
-            textoFavorito: "🩶"
-        })            
-        }
-        const esPeli = this.props.tipo == "movie";
-        this.setState(
-            {
-                esFavorito: !this.state.esFavorito,
-                textoFavorito: this.state.textoFavorito === "🩶" ? "Sacar de favoritos" : "🩶",
-                favs: this.state.textoFavorito === "🩶" ? "" : "noMostrar"
+            if (esPeli == false) {
+                if (favSeries === null) {
+                    let series = []
+                    series.push(id)
+                    let favString = JSON.stringify(series)
+                    localStorage.setItem("seriesFav", favString)
+                } else {
+                    let parseado = JSON.parse(favSeries)
+                    parseado.push(id)
+                    let favString = JSON.stringify(parseado)
+                    localStorage.setItem("seriesFav", favString)
+                }
+                this.setState({
+                    esFavorito: true,
+                    textoFavorito: "Sacar de favoritos"
+                })
             }
-        )
-    }
-    }
-    render(){
-        return(
-            <article class="single-card">
-                <img src={`https://image.tmdb.org/t/p/w500${this.props.data.poster_path}`} class="card-img-top"
-                    alt="..." />
-                <div class="cardBody">
-                    <h5 class="card-title">{this.props.tipo=="movie"?this.props.data.title:this.props.data.name}</h5>
-                    <button onClick={() => this.manejarDescripcion()} className="btn btn-primary">{this.state.textoBoton}</button>
-                    { this.state.mostrarDescripcion ? <p class="card-text">{this.props.data.overview} </p> : '' }
-                    <Link to={`/detalle/${this.props.tipo}/${this.props.data.id}`} class="btn btn-primary">Ver más</Link>
-                    <button className="favorito" onClick={() => this.botonFavorito()}>{this.state.textoFavorito} </button>
-                </div>
-            </article>
-        )
+
+            if (esPeli === true) {
+                if (favPelis === null) {
+                    let pelis = []
+                    pelis.push(id)
+                    let favString = JSON.stringify(pelis)
+                    localStorage.setItem("pelisFav", favString)
+                } else {
+                    let parseado = JSON.parse(favPelis)
+                    parseado.push(id)
+                    let favString = JSON.stringify(parseado)
+                    localStorage.setItem("pelisFav", favString)
+                }
+                this.setState({
+                    esFavorito: true,
+                    textoFavorito: "Sacar de favoritos"
+                })
+            }
+        } else {
+            if (esPeli == false) {
+                if (favSeries) {
+                    let parseado = JSON.parse(favSeries)
+                    let nuevoFiltro = parseado.filter(ids => ids != id)
+                    let favString = JSON.stringify(nuevoFiltro)
+                    localStorage.setItem("seriesFav", favString)
+                }
+                this.setState({
+                    esFavorito: false,
+                    textoFavorito: "🩶"
+                })
+            }
+        
+        if (esPeli == true) {
+            if (favPelis) {
+                let parseado = JSON.parse(favPelis);
+                let nuevoFiltro = parseado.filter(function (ids) { return ids != id; });
+                let favString = JSON.stringify(nuevoFiltro);
+                localStorage.setItem("pelisFav", favString);
+            }
+            this.setState({ esFavorito: false, textoFavorito: "🩶" });
+        }
     }
 }
+
+
+render() {
+    return (
+        <article class="single-card">
+            <img src={`https://image.tmdb.org/t/p/w500${this.props.data.poster_path}`} class="card-img-top"
+                alt="..." />
+            <div class="cardBody">
+                <h5 class="card-title">{this.props.tipo == "movie" ? this.props.data.title : this.props.data.name}</h5>
+                <button onClick={() => this.manejarDescripcion()} className="btn btn-primary">{this.state.textoBoton}</button>
+                {this.state.mostrarDescripcion ? <p class="card-text">{this.props.data.overview} </p> : ''}
+                <Link to={`/detalle/${this.props.tipo}/${this.props.data.id}`} class="btn btn-primary">Ver más</Link>
+                <button className="favorito" onClick={() => this.botonFavorito()}>{this.state.textoFavorito} </button>
+            </div>
+        </article>
+    )
+
+    }
+}
+
 
 export default Card;
